bcff3661990b36205b2497b847bb45316aa7fded
ethantrang <ethan.trang5521@gmail.com>
2024-08-10 17:09:57 +1000

    push again

--- src/app/login/github-button.tsx
+++ src/app/login/github-button.tsx
@@ -1,25 +1,28 @@
-"use client"
+"use client";
 
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Image from "next/image";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 
 export default function GitHubButton() {
     const supabase = createClientComponentClient<Database>();
 
-    async function handleSignIn() {
+    const handleSignIn = async () => {
         await supabase.auth.signInWithOAuth({
             provider: "github",
             options: {
-                redirectTo: "http://localhost:3000/auth/callback"
-            }
-        })
-    }
+                redirectTo: `${location.origin}/auth/callback`,
+            },
+        });
+    };
 
     return (
-        <button onClick={handleSignIn}
-            className="hover:bg-gray-800 p-8 rounded-xl">
-            <Image src="/github-mark-white.png" alt="Github Logo" width={100} height={100} />
-
+        <button onClick={handleSignIn} className="hover:bg-gray-800 p-8 rounded-xl">
+            <Image
+                src="/github-mark-white.png"
+                alt="GitHub logo"
+                width={100}
+                height={100}
+            />
         </button>
-    ) 
+    );
 }
